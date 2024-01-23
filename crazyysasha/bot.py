ca04cd019f0c31557c436ebf688d0ce765c183a9
Sasha Raimov <crazyysasha@gmail.com>
2024-01-23 19:46:21 +0500

    fix bot

--- crazyysasha/bot.py
+++ crazyysasha/bot.py
@@ -6,6 +6,8 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from dotenv import load_dotenv
 
+from crazyysasha.db import findUserByTelegramId
+
 
 
 
@@ -20,7 +22,7 @@ dp = Dispatcher();
 
 @dp.message(Command('start'))
 async def startHandler(message: Message, state: FSMContext) :
-    # if message.chat.id 
+    if findUserByTelegramId(message.chat.id):
 
         await message.answer("""бла бла бла - прикольная игра.
 
@@ -29,9 +31,9 @@ async def startHandler(message: Message, state: FSMContext) :
 /story - история игр
 /profile - профиль
 """);
-    # else:
-        # await state.set_state(RegisterState.requestName)
-        # await message.answer('э, напиши свое имя !');
+    else:
+        await state.set_state(RegisterState.requestName)
+        await message.answer('э, напиши свое имя !');
 
 async def main():
     await dp.start_polling(bot);
