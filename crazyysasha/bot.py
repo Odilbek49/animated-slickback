f06f0a93212649851c0aa8c2bf46128b38315bd6
Sasha Raimov <crazyysasha@gmail.com>
2024-01-27 19:39:59 +0500

    /start Fixes #1

--- crazyysasha/bot.py
+++ crazyysasha/bot.py
@@ -3,6 +3,7 @@ from os import getenv
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message
 from dotenv import load_dotenv
 
@@ -19,6 +20,9 @@ bot = Bot(token);
 
 dp = Dispatcher();
 
+
+class RegisterState(StatesGroup):
+    requestName = State();
 
 @dp.message(Command('start'))
 async def startHandler(message: Message, state: FSMContext) :
